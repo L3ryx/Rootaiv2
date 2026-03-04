8e949a93f8423f13cc0977ad4a251f6521c0d5d5
L3ryx <94659000+L3ryx@users.noreply.github.com>
2026-03-04 11:17:58 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,12 +1,29 @@
+const socket = io();
+let socketId = null;
+
+socket.on("connected", (data) => {
+  socketId = data.socketId;
+});
+
+socket.on("progress", (data) => {
+
+  const progressDiv = document.getElementById("progress");
+
+  const p = document.createElement("p");
+  p.textContent = data.message;
+
+  progressDiv.appendChild(p);
+
+});
+
 const form = document.getElementById("uploadForm");
-const resultsDiv = document.getElementById("results");
 
 form.addEventListener("submit", async (e) => {
+
   e.preventDefault();
 
-  resultsDiv.innerHTML = "Analyzing...";
-
   const formData = new FormData(form);
+  formData.append("socketId", socketId);
 
   const response = await fetch("/analyze", {
     method: "POST",
@@ -15,29 +32,5 @@ form.addEventListener("submit", async (e) => {
 
   const data = await response.json();
 
-  resultsDiv.innerHTML = "";
-
-  data.results.forEach(result => {
-
-    const card = document.createElement("div");
-    card.className = "card";
-
-    let html = `<h3>${result.image}</h3>`;
-
-    if (result.products.length > 0) {
-      result.products.forEach(product => {
-        html += `
-          <p><b>${product.title}</b></p>
-          <p>Price: ${product.price}</p>
-          <a href="${product.url}" target="_blank">Open</a>
-          <hr/>
-        `;
-      });
-    } else {
-      html += "<p>No products found</p>";
-    }
-
-    card.innerHTML = html;
-    resultsDiv.appendChild(card);
-  });
+  console.log(data);
 });
